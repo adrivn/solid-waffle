aca6a5735a18119ee1a35c142fadea3d6da50258
adrivn <adrian.emege@gmail.com>
2019-12-06 15:38:58 +0100

    Added Pandas, csv and lambda functions

--- run.py
+++ run.py
@@ -1,19 +1,29 @@
 import pycatastro
 from pyproj import Proj, transform
+import pandas
 
 # This invokes the wrapper
 cat = pycatastro.PyCatastro
 
+# File to load
+file_name = input("What's the name of the CSV file to load?: ")
+
+df = pandas.read_csv(file_name)
+
 # Fix the value to the variable
-losMunicipios = cat.ConsultaMunicipio('')
-lasProvincias = cat.ConsultaProvincia()
+# losMunicipios = cat.ConsultaMunicipio('')
+# lasProvincias = cat.ConsultaProvincia()
 
 latlong = cat.Consulta_CPMRC('','','','')
 
-x_coord = latlong['consulta_coordenadas']['coordenadas']['coord']['geo']['xcen']
-y_coord = latlong['consulta_coordenadas']['coordenadas']['coord']['geo']['ycen']
+df['result'] = df.apply(lambda x: cat.Consulta_CPMRC(0,0,0,x['RC']),axis=1)
 
-inProj = Proj(init='epsg:25830')
-outProj = Proj(init='epsg:4326')
-x2,y2 = transform(inProj,outProj,x_coord,y_coord)
-print(y2,x2)
+print(df['result'])
+
+#x_coord = latlong['consulta_coordenadas']['coordenadas']['coord']['geo']['xcen']
+#y_coord = latlong['consulta_coordenadas']['coordenadas']['coord']['geo']['ycen']
+
+#inProj = Proj(init='epsg:25830')
+#outProj = Proj(init='epsg:4326')
+#x2,y2 = transform(inProj,outProj,x_coord,y_coord)
+#print(y2,x2)
