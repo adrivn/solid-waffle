a08502968b5a0f1bd3ee96c9ccf9d879f8bb14b1
AM <adrian.emege@gmail.com>
2019-12-06 15:55:49 +0100

    Added Pandas, csv, lambda functions

--- run.py
+++ run.py
@@ -1,24 +1,19 @@
 import pycatastro
 from pyproj import Proj, transform
 import pandas
+import json
 
 # This invokes the wrapper
 cat = pycatastro.PyCatastro
 
 # File to load
 file_name = input("What's the name of the CSV file to load?: ")
-
+# Load the file onto a Pandas DataFrame
 df = pandas.read_csv(file_name)
 
-# Fix the value to the variable
-# losMunicipios = cat.ConsultaMunicipio('')
-# lasProvincias = cat.ConsultaProvincia()
-
-latlong = cat.Consulta_CPMRC('','','','')
-
+# Apply class method 'catastro' via lambda function
 df['result'] = df.apply(lambda x: cat.Consulta_CPMRC(0,0,0,x['RC']),axis=1)
-
-print(df['result'])
+print(df)
 
 #x_coord = latlong['consulta_coordenadas']['coordenadas']['coord']['geo']['xcen']
 #y_coord = latlong['consulta_coordenadas']['coordenadas']['coord']['geo']['ycen']
